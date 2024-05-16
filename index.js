eb43f00e856e6910b0e496508dc4f521b3f8a05b
anhhuy007 <imanhhuy007@gmail.com>
2024-05-16 21:43:23 +0700

    feat: remove error

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ app.get('/api/hello', (req, res) => {
     res.send('Hello, World!');
 });
 
-const bookRouter = require('book_router')
-
-app.use("/api/books", bookRouter)
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
