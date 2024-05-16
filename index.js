7401589e5ebbb9b27ebaec5d3f9851dca9a9a1b5
anhhuy007 <imanhhuy007@gmail.com>
2024-05-16 21:47:16 +0700

    feat: fetch books

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bookController = require('./book_controller');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,6 +8,8 @@ require('dotenv').config();
 app.get('/api/hello', (req, res) => {
     res.send('Hello, World!');
 });
+
+app.get('/api/books', bookController.getAll);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
