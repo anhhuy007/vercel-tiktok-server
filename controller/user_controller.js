ea540448900082d1857c4908ff2fb20611b3463b
anhhuy007 <imanhhuy007@gmail.com>
2024-05-30 11:55:15 +0700

    feat: fix route for user update and delete routes

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,4 +1,5 @@
 const postgres = require('../database/database');
+const { updateById } = require('./book_controller');
 
 const userController = {
     getAll: async(req, res) => {
@@ -35,7 +36,7 @@ const userController = {
         }
     },
 
-    update: async(req, res) => {
+    updateById: async(req, res) => {
         const { youtube_id, handle, name, subscribers, description, avatar_url, thumbnail_url, youtube_url } = req.body;
         try {
             const { rows } = postgres.query("UPDATE user_info SET youtube_id = $1, handle = $2, name = $3, subscribers = $4, description = $5, avatar_url = $6, thumbnail_url = $7, youtube_url = $8 WHERE id = $9 RETURNING *", [youtube_id, handle, name, subscribers, description, avatar_url, thumbnail_url, youtube_url, req.params.id]);
@@ -49,7 +50,7 @@ const userController = {
         }
     },
 
-    delete: async(req, res) => {
+    deleteById: async(req, res) => {
         try {
             const { rows } = postgres.query("DELETE FROM user_info WHERE id = $1 RETURNING *", [req.params.id]);
             if (rows[0]) {
