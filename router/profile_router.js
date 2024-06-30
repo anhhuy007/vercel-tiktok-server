02b74d6b04f4629ba9777390863a81c0d05ee21a
anhhuy007 <imanhhuy007@gmail.com>
2024-06-30 10:40:46 +0700

    fix: function name error bug

--- router/profile_router.js
+++ router/profile_router.js
@@ -4,7 +4,7 @@ const router = express.Router()
 // const profileController = require('../controller/profile_controller')
 const dbUploader = require('../database/upload_data')
 
-router.get("/upload/:id", dbUploader.uploadProfiles)
+router.get("/upload/:id", dbUploader.uploadUsers)
 
 // router.get('/info/:id', profileController.getProfileInfoById)
 // router.get('/latest/videos/:id', profileController.getLatestVideosById)
