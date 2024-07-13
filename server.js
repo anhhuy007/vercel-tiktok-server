f9b47c5dbc843030b2b50da234a6437770b8e66e
anhhuy007 <imanhhuy007@gmail.com>
2024-07-13 20:31:16 +0700

    feat: fix bugs

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express")
 const app = express()
 const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
-const path = require('path');
-const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
+const swaggerDocument = YAML.load('./swagger.yaml');
 require('dotenv').config()
 
 const AuthRouter = require("./router/auth_router")
@@ -11,7 +10,7 @@ const AuthRouter = require("./router/auth_router")
 const ProfileRouter = require("./router/profile_router")
 
 app.use(express.json())
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use("/api/v1/auth", AuthRouter) // for login and signup
 // app.use("/api/v1/feed", FeedRouter) // fetching short videos for the feed page
