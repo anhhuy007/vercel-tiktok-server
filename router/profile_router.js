dca5110f2db18fd15d60fc623ddf56c3653a5069
anhhuy007 <imanhhuy007@gmail.com>
2024-07-13 20:07:26 +0700

    feat: fix bugs

--- router/profile_router.js
+++ router/profile_router.js
@@ -4,8 +4,8 @@ const router = express.Router()
 const profileController = require('../controller/profile_controller')
 
 router.get('/info/:id', profileController.getProfileInfoById)
-router.get('/latest/videos/:id', profileController.getLatestVideosByUserId) // id, thumbnail_url, views
-router.get('/popular/videos/:id', profileController.getPopularVideosByUserId)
-router.get('/oldest/videos/:id', profileController.getOldestVideosByUserId)
+router.get('/videos/latest/:id', profileController.getLatestVideosByUserId) // id, thumbnail_url, views
+router.get('/videos/popular/:id', profileController.getPopularVideosByUserId)
+router.get('/videos/oldest/:id', profileController.getOldestVideosByUserId)
 
 module.exports = router
