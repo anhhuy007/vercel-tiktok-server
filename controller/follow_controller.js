4d43051c72cb384166ee47988e435781ddfaf80c
Nguyen Phuc Huy <124686333+HuyNguyen25@users.noreply.github.com>
2024-08-12 09:53:47 -0400

    feat: fix bugs

--- controller/follow_controller.js
+++ controller/follow_controller.js
@@ -5,11 +5,13 @@ const follow_controller = {
         try {
             const { follower_id, following_id } = req.body
             if (!follower_id || !following_id) return res.status(400).json({msg: "Please fill in all the fields"})
-            
+            const exist = await postgres.query("SELECT * FROM follower WHERE host_id = $1 AND follower_id = $2",[following_id, follower_id])
+            if (exist.rows.length > 0) return res.status(409).json({msg: "Can not follow anymore"})
+                
             const updateFollowerQuery = "INSERT INTO follower (host_id, follower_id) VALUES ($1, $2)"
             const updateFollowingQuery = "INSERT INTO following (host_id, following_id) VALUES ($1, $2)"
-            const updateFollowerUserInfoQuery = "UPDATE userinfo SET following = following + 1 WHERE id = $1"
-            const updateFollowingUserInfoQuery = "UPDATE userinfo SET follower = follower + 1 WHERE id = $1"
+            const updateFollowerUserInfoQuery = "UPDATE user_info SET following = following + 1 WHERE id = $1"
+            const updateFollowingUserInfoQuery = "UPDATE user_info SET follower = follower + 1 WHERE id = $1"
 
             await postgres.query(updateFollowerQuery, [following_id, follower_id])
             await postgres.query(updateFollowingQuery, [follower_id, following_id])
--- controller/like_controller.js
+++ controller/like_controller.js
@@ -6,7 +6,10 @@ const likeController = {
             const { video_id, liker_id } = req.body
             if (!video_id || !liker_id) return res.status(400).json({msg: "Please fill in all the fields"})
             
-            const likeDbQuery = "INSERT INTO like (video_id, liker_id) VALUES ($1, $2) RETURNING *"
+            const exist = await postgres.query("SELECT * FROM likes WHERE liker_id = $1 and video_id = $2", [liker_id, video_id])
+            if (exist.rows.length > 0) return res.status(409).json({msg: "Can not like anymore"})
+                
+            const likeDbQuery = "INSERT INTO likes (video_id, liker_id) VALUES ($1, $2) RETURNING *"
             const { rows } = await postgres.query(likeDbQuery, [video_id, liker_id])
             const videoDbQuery = "UPDATE video SET likes = likes + 1 WHERE id = $1"
             await postgres.query(videoDbQuery,[video_id])
