bdfe70203af987ea9de6c6ca587f07d835fd5cee
anhhuy007 <imanhhuy007@gmail.com>
2024-08-30 23:25:37 +0700

    feat: add sign up feature

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -21,17 +21,26 @@ const authController = {
     },
     signup: async(req, res) => {
         try {
-            const { email, password, username } = req.body
+            const { email, password, username, name } = req.body
 
-            if (!email || !password || !username) return res.status(400).json({msg: "Please fill in all the fields"})
+            if (!email || !password || !username || !name) return res.status(400).json({msg: "Please fill in all the fields"})
             
             const exists = await postgres.query("SELECT * FROM account WHERE email = $1", [email])
             if (exists.rows.length > 0) return res.status(409).json({msg: "Account already exists"})
-
+            
+            // insert into ACCOUNT table
             const hashedPassword = await bcrypt.hash(password, saltRounds)
-            const query = "INSERT INTO account (email, password, username) VALUES ($1, $2, $3) RETURNING *"
-            const { rows } = await postgres.query(query, [email, hashedPassword, username])
-            res.json({msg: "Signup successful", data: {id: rows[0].id, email: rows[0].email, username: rows[0].username}})
+            const query = "INSERT INTO account (username, email, password) VALUES ($1, $2, $3) RETURNING *"
+            let { rows } = await postgres.query(query, [username, email, hashedPassword])
+            const account = rows[0]
+            // insert into USER_INFO table
+            const handle = '@' + username
+            const defaultAvatar = 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png'
+            const defaultThumbnail = 'https://cdn.pixabay.com/photo/2023/02/03/05/11/youtube-background-7764170_1280.jpg'
+            const query2 = "INSERT INTO user_info (id, handle, name, avatar_url, thumbnail_url) VALUES ($1, $2, $3, $4, $5) RETURNING *"
+            rows = await postgres.query(query2, [account.id, handle, name, defaultAvatar, defaultThumbnail])
+
+            res.json({msg: "Signup successful", data: rows[0]})
         } catch (error) {
             console.log("Error: ", error)
             res.status(400).json({msg: error.msg})
@@ -52,12 +61,13 @@ const authController = {
                     const token = generateAccessToken({email: user.email, username: user.username})
                     const refreshToken = generateRefreshToken({email: user.email, username: user.username})
                     refreshTokens.push(refreshToken)
+
                     res.json({msg: "Login successfull", data: 
                         {
                             token: token, 
                             refresh_token: refreshToken, 
                             user: {
-                                id: user.id,
+                                id: user.id, 
                                 email: user.email, 
                                 username: user.username
                             }
@@ -94,6 +104,36 @@ const authController = {
     logout: async(req, res) => {
         refreshTokens = refreshTokens.filter(token => token !== req.body.refresh_token)
         res.sendStatus(204)
+    },
+    findEmail: async(req, res) => {
+        try {
+            const { email } = req.body
+            const query = "SELECT * FROM account WHERE email = $1"
+            const { rows } = await postgres.query(query, [email])
+            if (rows.length === 0) {
+                res.status(404).json({msg: "Account not found"})
+            } else {
+                res.json({msg: "Account found", data: rows[0]})
+            }
+        } catch (error) {
+            console.log("Error: ", error)
+            res.status(401).json({msg: error.msg})
+        }
+    },
+    findUsername: async(req, res) => {
+        try {
+            const { username } = req.body
+            const query = "SELECT * FROM account WHERE username = $1"
+            const { rows } = await postgres.query(query, [username])
+            if (rows.length === 0) {
+                res.status(404).json({msg: "Account not found"})
+            } else {
+                res.json({msg: "Account found", data: rows[0]})
+            }
+        } catch (error) {
+            console.log("Error: ", error)
+            res.status(401).json({msg: error.msg})
+        }
     }
 }
 
--- router/auth_router.js
+++ router/auth_router.js
@@ -8,6 +8,8 @@ const authController = require('../controller/auth_controller')
 router.post("/signup", authController.signup)
 router.post("/login", authController.login)
 router.post("/token", authController.token)
+router.post("findEmail", authController.findEmail)
+router.post("findUsername", authController.findUsername)
 router.delete("/logout", authController.logout)
 
 module.exports = router
