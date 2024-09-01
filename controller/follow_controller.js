e4dafa4e767186b925977468535b300a42ecbb87
Nguyen Phuc Huy <124686333+HuyNguyen25@users.noreply.github.com>
2024-09-01 12:12:22 -0400

    feat: get like and follow status

--- controller/follow_controller.js
+++ controller/follow_controller.js
@@ -29,6 +29,31 @@ const follow_controller = {
             console.log(err)
             res.status(400).json({msg: err.msg})
         }
+    },
+    getFollowStatus: async (req, res) => {
+        try {
+            const { follower_id, following_id } = req.body
+            if (!follower_id || !following_id) return res.status(400).json({msg: "Please fill in all the fields"})
+            
+            const exist = await postgres.query("SELECT * FROM follower WHERE host_id = $1 AND follower_id = $2", [following_id, follower_id])
+            if (exist.rows.length > 0) 
+                return res.json({
+                    msg: "OK",
+                    data: {
+                        followed: true
+                    }
+                })
+            else
+                return res.json({
+                    msg: "OK",
+                    data: {
+                        followed: false
+                    }
+                })
+        } catch (err) {
+            console.log(err)
+            res.status(400).json({msg: err.msg})
+        }
     }
 }
 
